ba824314fc203b61ea5fb908574b0cfa1aa6ed88
rutikeyone <rutikeyone@gmail.com>
2021-09-27 23:53:28 +0300

    Change layout

--- app/src/main/java/com/example/applibrary/MainActivity.java
+++ app/src/main/java/com/example/applibrary/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.applibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button seeAllBtn, currentlyReadingBooksBtn, alreadyReadBooksBtn, yourWishlistBtn, seeYourFavouriteBtn, aboutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        seeAllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllbooksActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView(){
+        seeAllBtn = findViewById(R.id.seeAllBooksBtn);
+        currentlyReadingBooksBtn = findViewById(R.id.currentlyReadingBooksBtn);
+        alreadyReadBooksBtn = findViewById(R.id.alreadyReadBooksBtn);
+        yourWishlistBtn = findViewById(R.id.youtWishlistBtn);
+        seeYourFavouriteBtn = findViewById(R.id.seeYourFavouritesBtn);
+        aboutBtn = findViewById(R.id.about);
     }
 }
